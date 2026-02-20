23cd41677bfcdd43585a7ec1158bb404d4ea050c
Hossam Hamad <hsh02928@gmail.com>
2026-02-20 14:44:26 +0200

    feat : add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div>
+      <h1>Footer</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem incidunt
+        sit nobis, praesentium laboriosam autem distinctio. Ducimus aspernatur
+        nihil, architecto officia reprehenderit quae repudiandae minima esse
+        ipsa accusamus? Animi, cumque.
+      </p>
+    </div>
+  );
+}
